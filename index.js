ae26b3b0c413a4d55058258e745bc8aa1152bbb2
santhosh kumar <thisis.santhosh777@gmail.com>
2023-01-26 11:35:18 +0000

    UPDATE Book API

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ app.get("/books/", async (request, response) => {
 });
 
 //Get Book API
-
 app.get("/books/:bookId", async (request, response) => {
   const { bookId } = request.params;
   const getBookQuery = `
@@ -55,7 +54,6 @@ app.get("/books/:bookId", async (request, response) => {
 });
 
 // POST (or) ADD Book API
-
 app.post("/books/", async (request, response) => {
   const bookDetails = request.body;
   const {
@@ -93,4 +91,44 @@ app.post("/books/", async (request, response) => {
   const dbResponse = await db.run(addBookQuery);
   const bookId = dbResponse.lastID;
   response.send({ bookId: bookId });
+});
+
+//PUT (or) UPDATE Book API
+
+app.put("/books/:bookId", async (request, response) => {
+  const { bookId } = request.params;
+  const bookDetails = request.body;
+  const {
+    title,
+    authorId,
+    rating,
+    ratingCount,
+    reviewCount,
+    description,
+    pages,
+    dateOfPublication,
+    editionLanguage,
+    price,
+    onlineStores,
+  } = bookDetails;
+  const updateBookQuery = `
+    UPDATE
+      book
+    SET
+      title='${title}',
+      author_id=${authorId},
+      rating=${rating},
+      rating_count=${ratingCount},
+      review_count=${reviewCount},
+      description='${description}',
+      pages=${pages},
+      date_of_publication='${dateOfPublication}',
+      edition_language='${editionLanguage}',
+      price= ${price},
+      online_stores='${onlineStores}'
+    WHERE
+      book_id = ${bookId};`;
+
+  await db.run(updateBookQuery);
+  response.send("Book Updated Successfully");
 });
